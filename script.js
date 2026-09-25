d4676afea6f2691bcebb37115447aa794ebfc7ce
Trouli <petroula.kambouri@gmail.com>
2026-09-25 17:38:58 +0100

    changes

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 
-let button = document.getElementById("button");
-let i = document.getElementById("i");
-
-
+let i = 0
+document.getElementById("timeSpentClicking").textContent = time
 
 function clickButton()
 {
-    i = i + 1;
+    i = i + 1
+    displayButtonClickNumber()
+    displayTimeSpentClicking()
+}
+
+function displayButtonClickNumber()
+{
+    document.getElementById("numberOfClicks").textContent = i
+}
+
+function displayTimeSpentClicking()
+{
+    let s = i * 0.1
+    document.getElementById("timeSpentClicking").textContent = "Time spent clicking button: " + Math.floor(s) + " seconds."
 }
